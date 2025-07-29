b6b974449967d6975e5ec353904eef6232d21353
Dario Camargo <dpcamargo@gmail.com>
2025-07-29 13:29:26 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,7 @@ if [ ! -d "ms-restaurante" ]; then
     git clone "$REPO_RESTAURANTE"
 fi
 
+GIST_RAW_URL="https://gist.githubusercontent.com/dpcamargo/ac8e8cc28ae0d5f9b3a7e42787e2db1d/raw/docker-compose.yaml"
+curl -fsSL "$GIST_RAW_URL" -o docker-compose.yaml
+
 docker compose up -d
